3e67e7104efb4f9d2be4ddc1c1cf80442e371ce1
ivan <ivan.v.ivanov.2019@elsys-bg.org>
2023-06-05 19:52:49 +0300

    Created Spring Boot project

---
+++ biznis/src/main/java/com/example/biznis/BiznisApplication.java
@@ -0,0 +1,13 @@
+package com.example.biznis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiznisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiznisApplication.class, args);
+	}
+
+}
---
+++ biznis/src/test/java/com/example/biznis/BiznisApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.biznis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiznisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
